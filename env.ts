5584929901982aefa84e2dd0fbb8d64902741935
alcantaradev <alcantara.isaque167@gmail.com>
2024-05-22 17:47:06 -0300

    chore: add notion env

--- env.ts
+++ env.ts
@@ -1,3 +1,8 @@
 export const env = {
-    NEXT_PUBLIC_GOOGLE_MAPS_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY as string
+    // Google Maps
+    NEXT_PUBLIC_GOOGLE_MAPS_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY as string,
+    
+    // Notion
+    NOTION_SECRET: process.env.NOTION_SECRET as string,
+    NOTION_POSITION_TABLE_ID: process.env.NOTION_POSITION_TABLE_ID as string,
 }

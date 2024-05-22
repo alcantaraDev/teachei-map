f863c0a87d109560690bff2b86750840217d441c
alcantaradev <alcantara.isaque167@gmail.com>
2024-05-22 11:11:11 -0300

    put: add map component props

--- components/google-maps/Map.tsx
+++ components/google-maps/Map.tsx
@@ -3,26 +3,23 @@
 import { env } from "@/env"
 import { APIProvider, Map as GoogleMap} from "@vis.gl/react-google-maps"
 import { ReactNode } from "react"
-
-export type coordinates = {
-    lat: number
-    lng: number
-}
+import { coordinates } from "./@types"
 
 export type MapProps = {
     children?: ReactNode
-    center: coordinates
+    classname?: string
+    defaultCenter: coordinates
     zoom: number
 }
 
-export function Map({ center, zoom, children }:MapProps) {
+export function Map({ classname, defaultCenter, zoom, children }:MapProps) {
     return (
         <>
             <APIProvider apiKey={env.NEXT_PUBLIC_GOOGLE_MAPS_KEY}>
                 <GoogleMap
-                style={{width: '90vw', height: '800px'}}
-                defaultCenter={{lat: 22.54992, lng: 0}}
-                defaultZoom={3}
+                className={classname}
+                defaultCenter={defaultCenter}
+                defaultZoom={zoom}
                 gestureHandling={'greedy'}
                 disableDefaultUI={true}
                 >

6709809dbabac29a4fadc937f17f078a354fc2cb
alcantaradev <alcantara.isaque167@gmail.com>
2024-05-22 13:54:26 -0300

    feat: map default center and zoom

--- components/google-maps/Map.tsx
+++ components/google-maps/Map.tsx
@@ -2,23 +2,37 @@
 
 import { env } from "@/env"
 import { APIProvider, Map as GoogleMap} from "@vis.gl/react-google-maps"
-import { ReactNode } from "react"
+import { ReactNode, useState } from "react"
 import { coordinates } from "./@types"
 
 export type MapProps = {
     children?: ReactNode
     classname?: string
-    defaultCenter: coordinates
-    zoom: number
+    defaultCenter?: coordinates
+    zoom?: number
 }
 
-export function Map({ classname, defaultCenter, zoom, children }:MapProps) {
+export function Map({ classname, defaultCenter, zoom=15, children }:MapProps) {
+    const [ userPosition, setUserPosition ] = useState<coordinates>({
+        lat: -10.926477432250977, 
+        lng: -37.07320022583008
+    })
+
+    navigator.geolocation.getCurrentPosition((pos) => {
+        setUserPosition({
+            lat: pos.coords.latitude,
+            lng: pos.coords.longitude
+        })
+    },(error) => {
+        console.error(error)
+    })
+
     return (
         <>
             <APIProvider apiKey={env.NEXT_PUBLIC_GOOGLE_MAPS_KEY}>
                 <GoogleMap
                 className={classname}
-                defaultCenter={defaultCenter}
+                defaultCenter={defaultCenter ?? userPosition}
                 defaultZoom={zoom}
                 gestureHandling={'greedy'}
                 disableDefaultUI={true}
